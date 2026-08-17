0ab7e8acd56aaf5f9da51c1dcd037c6161a2d5df
aguirreedwin162361 <aguirreedwin162361@gmail.com>
2026-08-17 12:27:28 -0600

    Corrección función sumar

--- calculadora.cs
+++ calculadora.cs
@@ -2,7 +2,7 @@ public static class Calculadora
 {
     public static int Sumar(int a, int b)
     {
-        return a - b;
+        return a + b;
     }
 
     public static int Restar(int a, int b)
