04167e9651882c55636e77c6d55af716bd0bd3ca
aguirreedwin162361 <aguirreedwin162361@gmail.com>
2026-08-17 12:26:12 -0600

    Agregar Calculadora basica

---
+++ calculadora.cs
@@ -0,0 +1,12 @@
+public static class Calculadora
+{
+    public static int Sumar(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int Restar(int a, int b)
+    {
+        return a - b;
+    }
+}
